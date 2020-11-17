fe76aa0ebca4c6cf80f73652121308990ecb7dea
AndrewDAntonio <github email address>
2020-11-17 16:07:28 -0500

    solution added

---
+++ solution.js
@@ -0,0 +1,24 @@
+/**
+ * @param {string} s
+ * @return {boolean}
+ */
+var isValid = function(s) {
+    const brackets = {
+        '{' : '}',
+        '(' : ')',
+        '[' : ']'
+    };
+    
+    const stack = [];
+    
+    for(i = 0; i < s.length; i++) {
+        if (s[i] === brackets[stack[stack.length - 1]]) {
+            stack.pop()
+            } else {
+            stack.push(s[i])
+        }
+    }
+    
+    
+    return !stack.length;
+};
